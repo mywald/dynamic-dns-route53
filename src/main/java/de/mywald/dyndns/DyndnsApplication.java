f649e6d724245675e78cada1c0378fc1249fed32
mywald <philip.maiwald@googlemail.com>
2017-08-24 18:10:35 +0200

    initial

---
+++ src/main/java/de/mywald/dyndns/DyndnsApplication.java
@@ -0,0 +1,12 @@
+package de.mywald.dyndns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyndnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyndnsApplication.class, args);
+	}
+}
